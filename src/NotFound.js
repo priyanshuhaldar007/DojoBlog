69e316e70b85a430a1160a44fe33f22c76689bba
priyanshuhaldar007 <priyanshu1843@gmail.com>
2022-05-26 12:26:50 +0530

    Updated 404 page

--- src/NotFound.js
+++ src/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
     return ( <div className="not-found">
         <h2>Sorry</h2>
         <p>That page cannot be found</p>
-        <Link to="/">Back to the HomePage...</Link>
+        <Link to="/">Back to the HomePage</Link>
     </div> );
 }
  
